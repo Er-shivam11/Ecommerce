ffbd109e475bb58792eee9271b14ba9c36460cba
Er-shivam11 <shivamnirmal.097@gmail.com>
2024-07-20 11:20:18 +0530

    docker-setup

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,6 +84,8 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 #         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+#docker setup
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
